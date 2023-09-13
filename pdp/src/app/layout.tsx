96bb28b1722dceb905003cff9d7be91da6d3380b
gustavoadlima <gustavo.a.l.v.e.sdl@gmail.com>
2023-09-13 17:41:11 -0300

    feat: first commit

---
+++ pdp/src/app/layout.tsx
@@ -0,0 +1,27 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({ 
+  subsets: ['latin'],
+  weight: ["400", "500","700"]
+})
+
+export const metadata: Metadata = {
+  title: 'Produtos',
+  description: 'Pagina de produtos',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={roboto.className}>
+        {children}
+      </body>
+    </html>
+  )
+}
